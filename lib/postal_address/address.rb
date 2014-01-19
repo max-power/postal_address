50d86321108aa28f210efc2ca506363c36a1dd40
Max Power <kevin.melchert@gmail.com>
2014-01-19 04:07:23 +0100

    remove attributes methods
    it’s not need by the formatters anymore

--- lib/postal_address/address.rb
+++ lib/postal_address/address.rb
@@ -12,13 +12,7 @@ module Postal
     alias_method :province=,    :state=
     alias_method :territory=,   :state=
     alias_method :administrative_area_level_1=, :state=
-  
-    def initialize(attrs={})
-      attrs.each do |attr, value|
-        self.public_send(:"#{attr}=", value) if self.respond_to?(:"#{attr}=")
-      end if attrs
-    end
-    
+        
     def country_code=(code)
       @country_code = Postal.sanitize(code)
     end
@@ -26,25 +20,25 @@ module Postal
     def country
       @country ||= Postal.country_names[country_code]
     end
-
+  
+    def initialize(attrs={})
+      attrs.each do |attr, value|
+        self.public_send(:"#{attr}=", value) if self.respond_to?(:"#{attr}=")
+      end if attrs
+    end
+    
     def to_h
-      attributes
-    end
-    
-    def to_s
-      AddressFormatter::Text.new(attributes).render
-    end
-        
-    def to_html(params={})
-      AddressFormatter::HTML.new(attributes).render(params)
-    end
-    
-    private
-    
-    def attributes
       Fields.each_with_object({}) do |key, hash|
         hash[key] = public_send(key)
       end
+    end
+
+    def to_s
+      AddressFormatter::Text.new(to_h).render
+    end
+        
+    def to_html(params={})
+      AddressFormatter::HTML.new(to_h).render(params)
     end
   end
 end
