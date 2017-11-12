4eb42946145ebb8fe8296418059f6a31e3ecbb75
Kevin <kevin.melchert@gmail.com>
2017-11-12 11:29:43 +0100

    simplify HTML#address_data

--- lib/postal_address/formatters/html.rb
+++ lib/postal_address/formatters/html.rb
@@ -1,7 +1,7 @@
 module Postal
   module AddressFormatter
     class HTML < Text
-      Schema   = 'http://schema.org/PostalAddress'
+      ItemType = 'http://schema.org/PostalAddress'
       ItemProp = {
         recipient: 'name',
         street:    'streetAddress',
@@ -12,23 +12,27 @@ module Postal
       }
 
       def render(**params)
-        tag = params.delete(:tag) || :p
-        cnt = super.gsub("\n", "<br>")
-        content_tag tag, cnt, params.merge(itemscope: nil, itemtype: Schema)
+        tagname = params.delete(:tag) || :p
+        content = super.gsub("\n", "<br>")
+        tag tagname, content, params.merge(itemscope: nil, itemtype: ItemType)
       end
 
       private
 
       def address_data
-        super.each_with_object({}) do |(field, value), hash|
-          hash[field] = value && content_tag(:span, value, itemprop: ItemProp[field])
-        end
-      end
-
-      def content_tag(tag, content='', attrs={})
-        "<#{tag}#{tag_attributes(attrs)}>#{content}</#{tag}>"
+        super.map do |key, value|
+          [key, tag_for(key, value)]
+        end.to_h
       end
       
+      def tag_for(key, value)
+        tag :span, value, itemprop: ItemProp[key] if value
+      end
+
+      def tag(name, content='', **attrs)
+        "<#{name}#{tag_attributes(attrs)}>#{content}</#{name}>"
+      end
+  
       def tag_attributes(attrs)
         attrs.map { |k,v| v.nil? ? k : %[#{k}="#{v}"] }.unshift('').join(' ')
       end
